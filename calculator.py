6d0e56b983d2660cf0b9c8f37774ee64dfb64f93
rpdeekshith <deekshitha@sriaranea.com>
2025-08-20 10:15:37 +0530

    cvsn reddy

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# calculator.py
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+def subtract(a: int, b: int) -> int:
+    return a - b
+
+if __name__ == "__main__":
+    print("2 + 3 =", add(2, 3))
+    print("5 - 2 =", subtract(5, 2))
